612d4dd9b4ac5feab6533b20928a217a1c9c6c06
Shannon Fisher <davine_chi@hotmail.com>
2020-07-24 18:40:52 -0500

    Add Direction class

---
+++ Direction.cs
@@ -0,0 +1,23 @@
+﻿
+namespace JustCombat
+{
+    public class Direction
+    {
+        private float _heading;
+
+        public Direction(float heading)
+        {
+            _heading = heading;
+        }
+
+        public float GetHeading()
+        {
+            return _heading;
+        }
+
+        public void SetHeading(float heading)
+        {
+            _heading = heading;
+        }
+    }
+}
