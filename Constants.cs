b4ad058117a4eb55bfb3735888bb72237e964a9f
Shannon Fisher <davine_chi@hotmail.com>
2020-07-26 14:42:59 -0500

    Add player walk speed constant

--- Constants.cs
+++ Constants.cs
@@ -7,5 +7,7 @@ namespace JustCombat
         public const int SCREEN_HEIGHT = 675;
         public const int PLAYER_WIDTH = 24;
         public const int PLAYER_HEIGHT = 32;
+
+        public const float PLAYER_SPEED_WALK = 2.0f;
     }
 }
