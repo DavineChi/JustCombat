76670fb6460a78b068ec8455fcd0a3eff4875771
Shannon Fisher <davine_chi@hotmail.com>
2020-07-26 20:02:12 -0500

    Add player animation speed constant

--- Constants.cs
+++ Constants.cs
@@ -8,6 +8,7 @@ namespace JustCombat
         public const int PLAYER_WIDTH = 24;
         public const int PLAYER_HEIGHT = 32;
 
+        public const float PLAYER_ANIMATION_SPEED = 0.22f;
         public const float PLAYER_SPEED_WALK = 2.0f;
     }
 }
