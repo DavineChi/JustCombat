fbe9bbb68ee471dbfbad41165de0da40b71dd117
Shannon Fisher <davine_chi@hotmail.com>
2020-08-03 00:35:36 -0500

    Add Friz Quadrata font to GameContent

--- GameContent.cs
+++ GameContent.cs
@@ -7,11 +7,13 @@ namespace JustCombat
     {
         public Texture2D FumikoImage { get; set; }
         public SpriteFont LabelFont { get; set; }
+        public SpriteFont GameFont { get; set; }
 
         public GameContent(ContentManager content)
         {
             FumikoImage = content.Load<Texture2D>("Fumiko");
             LabelFont = content.Load<SpriteFont>("Consolas14");
+            GameFont = content.Load<SpriteFont>("FrizQuadrataTTRegular");
         }
     }
 }
