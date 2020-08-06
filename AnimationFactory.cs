9c58df144c970eac2cb93539229a9c189cf19b9e
Shannon Fisher <davine_chi@hotmail.com>
2020-08-05 22:21:48 -0500

    Correct indices and complete the implementation

--- AnimationFactory.cs
+++ AnimationFactory.cs
@@ -1,16 +1,18 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 
 namespace JustCombat
 {
     public class AnimationFactory
     {
-        public static Animation CreateAnimationIdlePlayer()
+        public static Animation CreateAnimationIdlePlayer(SpriteSheet spriteSheet, int x, int y, int duration)
         {
-            Animation result = new Animation();
+            Animation result;
+            Texture2D[] frames;
 
             int additionalFrames = 36;
-            int[] baseSequence = { 32, 64, 0, 64, 32 };
+            int[] baseSequence = { 1, 2, 0, 2, 1 };
             int[] finalSequence = new int[baseSequence.Length + additionalFrames];
 
             for (int k = 0; k < baseSequence.Length + additionalFrames; k++)
@@ -22,9 +24,11 @@ namespace JustCombat
 
                 else
                 {
-                    finalSequence[k] = 32;
+                    finalSequence[k] = 1;
                 }
             }
+
+            frames = new Texture2D[finalSequence.Length];
 
             for (int i = 0; i < finalSequence.Length; i++)
             {
@@ -33,7 +37,11 @@ namespace JustCombat
                 // Idle player sprite farthest right on spritesheet:
                 // (24 pixels wide) * (16 sprites to the right) = 384
                 //result.AddFrame(new Rectangle(384, yOffset, Constants.PLAYER_WIDTH, Constants.PLAYER_HEIGHT), TimeSpan.FromSeconds(Constants.PLAYER_ANIMATION_SPEED));
+
+                frames[i] = spriteSheet.GetTexture(x, yOffset);
             }
+
+            result = new Animation(frames, duration);
 
             return result;
         }
