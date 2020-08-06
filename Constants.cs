7a5c668ed95862e0989be3d3158f5733711b305e
Shannon Fisher <davine_chi@hotmail.com>
2020-08-05 21:00:06 -0500

    Add RUN constant, change WALK value

--- Constants.cs
+++ Constants.cs
@@ -3,13 +3,14 @@ namespace JustCombat
 {
     public class Constants
     {
-        public const int SCREEN_WIDTH = 1200;
+        public const int SCREEN_WIDTH  = 1200;
         public const int SCREEN_HEIGHT = 675;
-        public const int PLAYER_WIDTH = 24;
+        public const int PLAYER_WIDTH  = 24;
         public const int PLAYER_HEIGHT = 32;
 
         public const float PLAYER_ANIMATION_SPEED = 0.22f;
-        public const float PLAYER_SPEED_WALK = 2.0f;
+        public const float PLAYER_SPEED_WALK = 1.0f;
+        public const float PLAYER_SPEED_RUN  = 2.0f;
 
         public const int MAXIMUM_PLAYER_LEVEL = 60;
     }
