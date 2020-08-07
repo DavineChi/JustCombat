ba727f1cc842a4c64f2dd65a06424716e31ff571
Shannon Fisher <davine_chi@hotmail.com>
2020-08-07 00:10:11 -0500

    Add getter and setter methods to PrimShape class

--- Primitives/PrimShape.cs
+++ Primitives/PrimShape.cs
@@ -13,6 +13,16 @@ namespace JustCombat
         protected float _thickness;
         protected int _layerDepth;
 
+        public float GetX() { return _x; }
+        public float GetY() { return _y; }
+        public float GetWidth()  { return _width;  }
+        public float GetHeight() { return _height; }
+
+        public void SetX(float x) { _x = x; }
+        public void SetY(float y) { _y = y; }
+        public void SetWidth(float width)   { _width = width;   }
+        public void SetHeight(float height) { _height = height; }
+
         public Color GetColor()
         {
             return _color;
