ee820faa10ba4bb0e610ede60ba68e81fc3ac0ab
Shannon Fisher <davine_chi@hotmail.com>
2020-09-22 17:20:18 -0500

    Provide implementation for Panel subclasses

--- Panels/CharacterPanel.cs
+++ Panels/CharacterPanel.cs
@@ -1,8 +1,29 @@
-﻿
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 namespace JustCombat.Panels
 {
     public class CharacterPanel : Panel
     {
-        
+        public CharacterPanel(string id, int x, int y, int width, int height) :
+            this(id, x, y, width, height, Color.White)
+        {
+        }
+
+        public CharacterPanel(string id, int x, int y, int width, int height, Color color)
+        {
+            _id = id;
+            _x = x;
+            _y = y;
+            _width = width;
+            _height = height;
+
+            _panel = PrimRectangle.Create(x, y, width, height, color);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_panel, new Vector2(_x, _y), Color.White);
+        }
     }
 }
