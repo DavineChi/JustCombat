4dbe6169e6aaa307083a931360a322a1a453b931
Shannon Fisher <davine_chi@hotmail.com>
2020-09-27 01:26:21 -0500

    Add UserInterface class

---
+++ UI/UserInterface.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace JustCombat.UI
+{
+    public class UserInterface
+    {
+        private static UserInterface _userInterface = null;
+
+        private Player _player;
+
+        private Texture2D _topBarBackpanel;
+
+        private ActorInfoCard _playerInfoCard;
+        private ActorInfoCard _targetInfoCard;
+
+        private SpriteFont _font;
+
+        protected UserInterface()
+        {
+            _player = Player.Instance();
+            _topBarBackpanel = JustCombat.gameContent.TopBarBackpanel;
+            _playerInfoCard = new ActorInfoCard(_player);
+            _font = JustCombat.gameContent.GameFont;
+        }
+
+        public static UserInterface Instance()
+        {
+            if (_userInterface == null)
+            {
+                _userInterface = new UserInterface();
+            }
+
+            return _userInterface;
+        }
+
+        public ActorInfoCard GetPlayerInfoCard()
+        {
+            return _playerInfoCard;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _playerInfoCard.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_topBarBackpanel, new Vector2(0.0f, 0.0f), Color.White);
+
+            _playerInfoCard.Draw(spriteBatch);
+
+            spriteBatch.DrawString(_font, "X: " + _player.GetX() + ", Y: " + _player.GetY(), new Vector2(550.0f, 10.0f), Color.White);
+            spriteBatch.DrawString(_font, DateTime.Now.ToString(), new Vector2(550.0f, 30.0f), Color.White);
+
+            //spriteBatch.DrawString(_font, "Health " + _player.GetHitPoints().ToString() + " / " + _player.GetMaxHitPoints().ToString(), new Vector2(744.0f, 6.0f), Color.White);
+            spriteBatch.DrawString(_font, "       Player State: " + _player.GetState().ToString(), new Vector2(800.0f, 10.0f), Color.White);
+            spriteBatch.DrawString(_font, "HealthBar State: " + _playerInfoCard.GetHealthBar().GetState().ToString(), new Vector2(800.0f, 30.0f), Color.White);
+            spriteBatch.DrawString(_font, "            Heading: " + _player.GetDirection().GetHeading().ToString(), new Vector2(800.0f, 50.0f), Color.White);
+        }
+    }
+}
