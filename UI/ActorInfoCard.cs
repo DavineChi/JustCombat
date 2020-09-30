36319f43377e802c1330dc730b7e1711bcc9ffee
Shannon Fisher <davine_chi@hotmail.com>
2020-09-29 20:36:12 -0500

    Re-position text, use constants, change font

--- UI/ActorInfoCard.cs
+++ UI/ActorInfoCard.cs
@@ -10,6 +10,10 @@ namespace JustCombat.UI
         protected HealthBar _healthBar;
         protected ManaBar _manaBar;
 
+        private const int FILL_BAR_LEFT   = 24;
+        private const int FILL_BAR_WIDTH  = 182;
+        private const int FILL_BAR_HEIGHT = 8;
+
         private Vector2 _position = new Vector2(16.0f, 4.0f);
         private Texture2D _card;
         private SpriteFont _font;
@@ -23,9 +27,9 @@ namespace JustCombat.UI
             _card = JustCombat.gameContent.ActorInfoCard;
             _name = actor.GetName();
             _level = actor.GetLevel().ToString();
-            _healthBar = new HealthBar(23, 57, 172, 6);
-            _manaBar = new ManaBar(23, 69, 172, 6);
-            _font = JustCombat.gameContent.GameFont;
+            _healthBar = new HealthBar(FILL_BAR_LEFT, 66, FILL_BAR_WIDTH, FILL_BAR_HEIGHT);
+            _manaBar = new ManaBar(FILL_BAR_LEFT, 80, FILL_BAR_WIDTH, FILL_BAR_HEIGHT);
+            _font = JustCombat.gameContent.FontConsolas13;
         }
 
         public Texture2D GetTexture()
@@ -73,9 +77,9 @@ namespace JustCombat.UI
             string health = _actor.GetHitPoints().ToString() + " / " + _actor.GetMaxHitPoints().ToString();
 
             spriteBatch.Draw(_card, _position, Color.White);
-            spriteBatch.DrawString(_font, name, new Vector2(28.0f, 9.0f), Color.White);
-            spriteBatch.DrawString(_font, level, new Vector2(29.0f, 33.0f), Color.White);
-            spriteBatch.DrawString(_font, health, new Vector2(75.0f, 33.0f), Color.White);
+            spriteBatch.DrawString(_font, name, new Vector2(30.0f, 13.0f), Color.White);
+            spriteBatch.DrawString(_font, level, new Vector2(29.0f, 40.0f), Color.White);
+            spriteBatch.DrawString(_font, health, new Vector2(75.0f, 40.0f), Color.White);
 
             _healthBar.Draw(spriteBatch);
             _manaBar.Draw(spriteBatch);
