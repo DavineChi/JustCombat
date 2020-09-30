7ffe15eb0c49c8fff3c7fe287949ee987ec01bf3
Shannon Fisher <davine_chi@hotmail.com>
2020-09-29 20:24:53 -0500

    Add CooldownTimer class

---
+++ CooldownTimer.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace JustCombat
+{
+    public class CooldownTimer
+    {
+        private float _timerValue;
+        private float _duration;
+        private bool _running = false;
+
+        private GameTime _gameTime;
+
+        public CooldownTimer()
+        {
+            _duration = 0;
+        }
+
+        public CooldownTimer(float timerValue)
+        {
+            _timerValue = timerValue;
+            _duration = 0;
+        }
+
+        public float GetDuration()
+        {
+            return _duration;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (_running)
+            {
+                _gameTime = gameTime;
+                _duration = _duration - (float)(_gameTime.ElapsedGameTime.TotalSeconds);
+
+                if (_duration <= 0)
+                {
+                    _duration = 0;
+                    _running = false;
+                }
+            }
+        }
+
+        public void Start()
+        {
+            this.Reset();
+
+            _running = true;
+        }
+
+        public void Start(float duration)
+        {
+            _duration = duration;
+            _running = true;
+        }
+
+        public void Reset()
+        {
+            _duration = _timerValue;
+        }
+
+        public bool IsRunning()
+        {
+            return _running;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            if (_gameTime == null)
+            {
+                result = "null";
+            }
+
+            else
+            {
+                result = _duration.ToString("0.0");
+            }
+
+            return result;
+        }
+    }
+}
