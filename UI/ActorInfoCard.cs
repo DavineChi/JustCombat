f5c75d7e9a4dcb13509c51789bffd5fa55f973c1
Shannon Fisher <davine_chi@hotmail.com>
2020-10-01 01:23:37 -0500

    Allow relative position of text

--- UI/ActorInfoCard.cs
+++ UI/ActorInfoCard.cs
@@ -14,21 +14,33 @@ namespace JustCombat.UI
         private const int FILL_BAR_WIDTH  = 182;
         private const int FILL_BAR_HEIGHT = 8;
 
-        private Vector2 _position = new Vector2(16.0f, 4.0f);
+        private int _xPosition;
+        private int _yPosition;
+
+        private Vector2 _position;
         private Texture2D _card;
         private SpriteFont _font;
 
         private Actor _actor;
 
-        public ActorInfoCard(Actor actor)
+        public ActorInfoCard(Actor actor) :
+            this(actor, new Vector2(16.0f, 4.0f))
+        {
+        }
+
+        public ActorInfoCard(Actor actor, Vector2 position)
         {
             _actor = actor;
+            _position = position;
+
+            _xPosition = (int)(position.X);
+            _yPosition = (int)(position.Y);
 
             _card = JustCombat.gameContent.ActorInfoCard;
             _name = actor.GetName();
             _level = actor.GetLevel().ToString();
-            _healthBar = new HealthBar(FILL_BAR_LEFT, 66, FILL_BAR_WIDTH, FILL_BAR_HEIGHT);
-            _manaBar = new ManaBar(FILL_BAR_LEFT, 80, FILL_BAR_WIDTH, FILL_BAR_HEIGHT);
+            _healthBar = new HealthBar((_xPosition + 8), 66, FILL_BAR_WIDTH, FILL_BAR_HEIGHT);
+            _manaBar = new ManaBar((_xPosition + 8), 80, FILL_BAR_WIDTH, FILL_BAR_HEIGHT);
             _font = JustCombat.gameContent.FontConsolas13;
         }
 
@@ -77,9 +89,9 @@ namespace JustCombat.UI
             string health = _actor.GetHitPoints().ToString() + " / " + _actor.GetMaxHitPoints().ToString();
 
             spriteBatch.Draw(_card, _position, Color.White);
-            spriteBatch.DrawString(_font, name, new Vector2(30.0f, 13.0f), Color.White);
-            spriteBatch.DrawString(_font, level, new Vector2(29.0f, 40.0f), Color.White);
-            spriteBatch.DrawString(_font, health, new Vector2(75.0f, 40.0f), Color.White);
+            spriteBatch.DrawString(_font, name, new Vector2((_xPosition + 14.0f), (13.0f)), Color.White);
+            spriteBatch.DrawString(_font, level, new Vector2((_xPosition + 13.0f), (40.0f)), Color.White);
+            spriteBatch.DrawString(_font, health, new Vector2((_xPosition + 59.0f), (40.0f)), Color.White);
 
             _healthBar.Draw(spriteBatch);
             _manaBar.Draw(spriteBatch);
