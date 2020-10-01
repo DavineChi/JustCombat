807c6654a9ec5652987b5532e377f7638dae022a
Shannon Fisher <davine_chi@hotmail.com>
2020-10-01 01:23:56 -0500

    Implement TargetingSystem class

--- TargetingSystem.cs
+++ TargetingSystem.cs
@@ -1,34 +1,57 @@
-﻿
+﻿using JustCombat.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 namespace JustCombat
 {
     public class TargetingSystem
     {
-        private static TargetingSystem _targetingSystem = null;
+        //private static TargetingSystem _targetingSystem = null;
 
+        private ActorInfoCard _targetInfoCard;
         private Entity _currentTarget;
 
-        protected TargetingSystem()
+        public TargetingSystem()
         {
         }
 
-        public static TargetingSystem Instance()
-        {
-            if (_targetingSystem == null)
-            {
-                _targetingSystem = new TargetingSystem();
-            }
+        //public static TargetingSystem Instance()
+        //{
+        //    if (_targetingSystem == null)
+        //    {
+        //        _targetingSystem = new TargetingSystem();
+        //    }
 
-            return _targetingSystem;
-        }
+        //    return _targetingSystem;
+        //}
 
         public void Acquire(Entity target)
         {
             _currentTarget = target;
+
+            if (target is Actor)
+            {
+                _targetInfoCard = new ActorInfoCard((Actor)(_currentTarget), new Vector2(244.0f, 4.0f));
+            }
         }
 
         public void Release()
         {
             _currentTarget = null;
+            _targetInfoCard = null;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (_targetInfoCard != null)
+            {
+                _targetInfoCard.Draw(spriteBatch);
+            }
         }
 
         public override string ToString()
@@ -42,7 +65,7 @@ namespace JustCombat
 
             else
             {
-                selected = _currentTarget.ToString();
+                selected = _currentTarget.GetName();
             }
 
             return selected;
