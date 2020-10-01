989c466f1e680b1c1aee141af16316e64c0e4eb2
Shannon Fisher <davine_chi@hotmail.com>
2020-10-01 01:24:57 -0500

    Add InfoPanel class

---
+++ Panels/InfoPanel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace JustCombat.Panels
+{
+    public class InfoPanel : Panel
+    {
+        public InfoPanel(int x, int y, int width, int height, Color color)
+        {
+            _x = x;
+            _y = y;
+            _width = width;
+            _height = height;
+            _color = color;
+            _panel = PrimRectangle.Create(x, y, width, height, color);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_panel, new Vector2(_x, _y), Color.White);
+            spriteBatch.DrawString(JustCombat.gameContent.FontConsolas10, "test", new Vector2(_x + 4, _y + 4), Color.White);
+        }
+    }
+}
