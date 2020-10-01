c0b3bb2d1ba8187999e04719de6155b0458dd953
Shannon Fisher <davine_chi@hotmail.com>
2020-10-01 01:27:21 -0500

    Add Spell and Fireball classes

---
+++ Spells/Fireball.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace JustCombat.Spells
+{
+    public class Fireball : Spell
+    {
+        public Fireball()
+        {
+            _rank = 1;
+            _castTime = 1.5f;
+            _timer = new CooldownTimer(_castTime);
+            _manaCost = 10.0f; // TODO: change to 7% base mana
+            _damage = 27;
+        }
+
+        // TODO: implementation
+        public override void Cast()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Spells/Spell.cs
@@ -0,0 +1,14 @@
+﻿
+namespace JustCombat.Spells
+{
+    public abstract class Spell
+    {
+        protected int _rank;
+        protected float _castTime;
+        protected CooldownTimer _timer;
+        protected float _manaCost;
+        protected int _damage;
+
+        public abstract void Cast();
+    }
+}
