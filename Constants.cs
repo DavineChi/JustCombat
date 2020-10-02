7497804e56b18ffb0bb76ddfa2cf185828c3169f
Shannon Fisher <davine_chi@hotmail.com>
2020-10-02 01:02:12 -0500

    Increase the drawable screen height

--- Constants.cs
+++ Constants.cs
@@ -4,7 +4,7 @@ namespace JustCombat
     public class Constants
     {
         public const int SCREEN_WIDTH  = 1200;
-        public const int SCREEN_HEIGHT = 675;
+        public const int SCREEN_HEIGHT = 780;
         public const int PLAYER_WIDTH  = 24;
         public const int PLAYER_HEIGHT = 32;
 
