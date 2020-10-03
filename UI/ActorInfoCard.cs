3c963a77b9a87b4ee8fd7ba971c35181b34f29df
Shannon Fisher <davine_chi@hotmail.com>
2020-10-03 00:12:21 -0500

    Clean up ActorInfoCard class

--- UI/ActorInfoCard.cs
+++ UI/ActorInfoCard.cs
@@ -3,14 +3,11 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace JustCombat.UI
 {
-    public class ActorInfoCard
+    public class ActorInfoCard : IDrawable
     {
-        protected string _name;
-        protected string _level;
         protected HealthBar _healthBar;
         protected ManaBar _manaBar;
 
-        private const int FILL_BAR_LEFT   = 24;
         private const int FILL_BAR_WIDTH  = 182;
         private const int FILL_BAR_HEIGHT = 8;
 
@@ -37,36 +34,9 @@ namespace JustCombat.UI
             _yPosition = (int)(position.Y);
 
             _card = JustCombat.gameContent.ActorInfoCard;
-            _name = actor.GetName();
-            _level = actor.GetLevel().ToString();
-            _healthBar = new HealthBar((_xPosition + 8), 66, FILL_BAR_WIDTH, FILL_BAR_HEIGHT);
-            _manaBar = new ManaBar((_xPosition + 8), 80, FILL_BAR_WIDTH, FILL_BAR_HEIGHT);
+            _healthBar = new HealthBar((_xPosition + 8), 66, FILL_BAR_WIDTH, FILL_BAR_HEIGHT, actor);
+            _manaBar = new ManaBar((_xPosition + 8), 80, FILL_BAR_WIDTH, FILL_BAR_HEIGHT, actor);
             _font = JustCombat.gameContent.FontConsolas13;
-        }
-
-        public Texture2D GetTexture()
-        {
-            return _card;
-        }
-
-        public string GetName()
-        {
-            return _name;
-        }
-
-        public string GetLevel()
-        {
-            return _level;
-        }
-
-        public void SetLevel(int level)
-        {
-            SetLevel(level.ToString());
-        }
-
-        public void SetLevel(string level)
-        {
-            _level = level;
         }
 
         public HealthBar GetHealthBar()
@@ -76,10 +46,8 @@ namespace JustCombat.UI
 
         public void Update(GameTime gameTime)
         {
-            _level = _actor.GetLevel().ToString();
-
-            _healthBar.Update(_actor, gameTime);
-            _manaBar.Update(_actor, gameTime);
+            _healthBar.Update(gameTime);
+            _manaBar.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -89,6 +57,7 @@ namespace JustCombat.UI
             string health = _actor.GetHitPoints().ToString() + " / " + _actor.GetMaxHitPoints().ToString();
 
             spriteBatch.Draw(_card, _position, Color.White);
+
             spriteBatch.DrawString(_font, name, new Vector2((_xPosition + 14.0f), (13.0f)), Color.White);
             spriteBatch.DrawString(_font, level, new Vector2((_xPosition + 13.0f), (40.0f)), Color.White);
             spriteBatch.DrawString(_font, health, new Vector2((_xPosition + 59.0f), (40.0f)), Color.White);
