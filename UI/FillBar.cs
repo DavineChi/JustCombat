cb42953f505f47aa57e0034e0331b2506dbb739d
Shannon Fisher <davine_chi@hotmail.com>
2020-10-04 23:33:07 -0500

    Remove unused States and CooldownTimer
    Also adds Getter and Setter for _actor field.

--- UI/FillBar.cs
+++ UI/FillBar.cs
@@ -3,12 +3,8 @@ namespace JustCombat.UI
 {
     public class FillBar
     {
-        public enum State { EMPTY, FULL, COMBAT, REGEN };
-
-        protected State _state;
         protected float _width;
         protected PrimRectangle _bar;
-        protected CooldownTimer _timer;
         protected Actor _actor;
 
         public FillBar(int xPosition, int yPosition, int width, int height, Actor actor)
@@ -18,6 +14,8 @@ namespace JustCombat.UI
             _actor = actor;
         }
 
-        public State GetState() { return _state; }
+        public Actor GetActor() { return _actor; }
+
+        public void SetActor(Actor actor) { _actor = actor; }
     }
 }
