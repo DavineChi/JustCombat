2fd33d3cbc6c7ec51e9461f763fd95d458ba4532
Shannon Fisher <davine_chi@hotmail.com>
2020-10-04 23:39:46 -0500

    Add name parameter and method overloads

--- SpriteSheet.cs
+++ SpriteSheet.cs
@@ -18,6 +18,11 @@ namespace JustCombat
 
         public static Texture2D GetTexture(Texture2D texture, int x, int y, int width, int height)
         {
+            return SpriteSheet.GetTexture("", texture, x, y, width, height);
+        }
+
+        public static Texture2D GetTexture(string name, Texture2D texture, int x, int y, int width, int height)
+        {
             Texture2D result = new Texture2D(JustCombat.graphics.GraphicsDevice, width, height);
             Rectangle cropRectangle = new Rectangle((x * width), (y * height), width, height);
             Color[] colorData = new Color[width * height];
@@ -25,17 +30,29 @@ namespace JustCombat
             texture.GetData(0, cropRectangle, colorData, 0, colorData.Length);
             result.SetData(colorData);
 
+            result.Name = name;
+
             return result;
         }
 
         public Texture2D GetTexture(int x, int y)
         {
-            return SpriteSheet.GetTexture(_texture, x, y, _tileWidth, _tileHeight);
+            return SpriteSheet.GetTexture("", _texture, x, y, _tileWidth, _tileHeight);
+        }
+
+        public Texture2D GetTexture(string name, int x, int y)
+        {
+            return SpriteSheet.GetTexture(name, _texture, x, y, _tileWidth, _tileHeight);
         }
 
         public Texture2D GetTexture(int x, int y, int width, int height)
         {
-            return SpriteSheet.GetTexture(_texture, x, y, width, height);
+            return SpriteSheet.GetTexture("", _texture, x, y, width, height);
+        }
+
+        public Texture2D GetTexture(string name, int x, int y, int width, int height)
+        {
+            return SpriteSheet.GetTexture(name, _texture, x, y, width, height);
         }
     }
 }
