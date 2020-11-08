8571cbd80e5784ceb749a3c131af90b3b7595c74
Shannon Fisher <davine_chi@hotmail.com>
2020-11-08 14:11:19 -0600

    Correct sprite rendering issue, clean up code

--- DrawOrderManager.cs
+++ DrawOrderManager.cs
@@ -13,19 +13,16 @@ namespace JustCombat
 
         private static Wraith _wraithOne = JustCombat.WraithOne;
         private static Actor _wraithTwo = JustCombat.WraithTwo;
-
-        private static float front = 0.0f;
-        private static float back  = 1.0f;
-
+        
         public static void Draw(SpriteBatch spriteBatch)
         {
             OrthographicCamera camera = JustCombat.WorldCamera;
             Matrix matrix = camera.GetViewMatrix();
 
-            float ellipseX = _player.GetX() + ((Constants.PLAYER_WIDTH  * Constants.SPRITE_SCALE) / 2);
-            float ellipseY = _player.GetY() +  (Constants.PLAYER_HEIGHT * Constants.SPRITE_SCALE);
+            //float ellipseX = _player.GetX() + ((Constants.PLAYER_WIDTH  * Constants.SPRITE_SCALE) / 2);
+            //float ellipseY = _player.GetY() +  (Constants.PLAYER_HEIGHT * Constants.SPRITE_SCALE);
 
-            Vector2 playerEllipse = new Vector2(ellipseX, (ellipseY - 2));
+            //Vector2 playerEllipse = new Vector2(ellipseX, (ellipseY - 2));
 
             _mapRenderer.Draw(matrix, null, null, 0);
 
@@ -33,18 +30,18 @@ namespace JustCombat
             {
                 Actor actor = (Actor)(entity);
 
+                float padding = actor.GetHeight();
                 float bottomY = actor.GetY() + actor.GetHeight();
-                float offsetY = Constants.SCREEN_HEIGHT - bottomY;
+                float offsetY = (Constants.SCREEN_HEIGHT + padding) - bottomY;
 
                 Vector2 worldCoords  = camera.ScreenToWorld(0, offsetY);
 
-                float layer = worldCoords.Y;
-
-                layer = (layer / 1000.0f);
+                // A layer depth of 0.0f is front-most, 1.0f is back-most.
+                float layer = (worldCoords.Y / 1000.0f);
 
                 if (actor is Player)
                 {
-                    spriteBatch.DrawEllipse(playerEllipse, new Vector2(24, 10), 10, Color.White, 1, 1);
+                    //spriteBatch.DrawEllipse(playerEllipse, new Vector2(24, 10), 10, Color.White, 1, 1);
                     _player.Draw(spriteBatch, layer);
                 }
 
